f14d4f90c68562105a1877aec416333e55ca4f3d
Samuel Burnham <samuel.burnham@gmail.com>
2023-04-18 17:33:09 -0400

    Test simpler setup

--- start.sh
+++ start.sh
@@ -8,9 +8,13 @@ echo "ACCESS_TOKEN ${ACCESS_TOKEN}"
 
 REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
 
+echo "Reg token created"
+
 cd /home/docker/actions-runner
 
 ./config.sh --url https://github.com/${REPOSITORY} --token ${REG_TOKEN}
+
+echo "Config complete"
 
 cleanup() {
     echo "Removing runner..."
