b6c19e6f6a557440b1cd0a0778a021bae46c6492
Samuel Burnham <45365069+samuelburnham@users.noreply.github.com>
2023-10-23 10:12:43 -0400

    Update tutorial from Paperspace test results (#17)

--- scripts/server_files.sh
+++ scripts/server_files.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # E.g. ../test-runner/gh-actions-runner*
-scp /path/to/runner/gh-actions-runner* root@<ip_addr>:/root
-
+scp /path/to/runner/gh-actions-runner* <user>@<ip_addr>:/path/to/home
 
 # For GPU setup, replace setup.sh with gpu_setup.sh below
-scp setup.sh install.sh remove.sh runner_cleanup.sh docker_cleanup.sh root@<ip_addr>:/root
+scp setup.sh install.sh remove.sh runner_cleanup.sh docker_cleanup.sh <user>@<ip_addr>:/path/to/home
