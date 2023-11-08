4dcafca3803d585a99b0ce6e894254e5a17ce01f
Samuel Burnham <45365069+samuelburnham@users.noreply.github.com>
2023-11-08 12:37:12 -0500

    Move runner labels to `.env` and add unique identifiers (#20)
    * Disambiguate runners by adding unique/useful labels
    
    * Move labels from compose file to .env
    
    * Add more labels and clean fix `.env` loading
    
    * Simplify labels to only include `runs-on` selector values

--- scripts/entrypoint-wrapper.sh
+++ scripts/entrypoint-wrapper.sh
@@ -8,8 +8,19 @@ mkdir -p /var/tmp/runner/_work
 RUNNER_DATE=$(date +"%y-%m-%d")
 head /dev/urandom | tr -dc a-f0-9 | head -c 13 | sed 's/$/-'$RUNNER_DATE'/' > /var/tmp/runner/_work/runner_id.txt
 _RUNNER_ID="$(cat /var/tmp/runner/_work/runner_id.txt)"
+# The name is a bit long, but there's no other way to give the runner a unique name when scaled with docker-compose
 RUNNER_NAME="${RUNNER_NAME}-${_RUNNER_ID}"
 RUNNER_WORKDIR="${RUNNER_WORKDIR}/${_RUNNER_ID}"
 
+# Get basic hardware info and append to labels
+LABELS=${LABELS},$(uname -s),$(uname -m)
+if [ $(command -v nvidia-smi) ]; then
+        LABELS=${LABELS},"gpu",$(nvidia-smi --query-gpu=gpu_name --format=csv,noheader,nounits | tail -n 1)
+fi
+# Add label for workdir/unique ID
+LABELS=${LABELS},${_RUNNER_ID}
+
+echo "Runner labels: $LABELS"
 echo "Runner workdir: $RUNNER_WORKDIR"
+
 exec /entrypoint.sh $@
