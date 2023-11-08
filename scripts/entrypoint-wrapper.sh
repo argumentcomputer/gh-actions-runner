76973c45734c36b4ac2cf6053dd1a25e6788d823
Samuel Burnham <45365069+samuelburnham@users.noreply.github.com>
2023-11-07 21:49:35 -0500

    Add more labels and clean fix `.env` loading

--- scripts/entrypoint-wrapper.sh
+++ scripts/entrypoint-wrapper.sh
@@ -5,20 +5,23 @@
 # For peace of mind that the workdir exists
 mkdir -p /var/tmp/runner/_work
 # Generate a unique, persistent id for each runner directory
-RUNNER_DATE=$(date +"%m-%d-%y")
+RUNNER_DATE=$(date +"%y-%m-%d")
 head /dev/urandom | tr -dc a-f0-9 | head -c 13 | sed 's/$/-'$RUNNER_DATE'/' > /var/tmp/runner/_work/runner_id.txt
 _RUNNER_ID="$(cat /var/tmp/runner/_work/runner_id.txt)"
+# The name is a bit long, but there's no other way to give the runner a unique name with a scaled docker-compose runner
 RUNNER_NAME="${RUNNER_NAME}-${_RUNNER_ID}"
 RUNNER_WORKDIR="${RUNNER_WORKDIR}/${_RUNNER_ID}"
 
 # Get hardware info and append to labels
-LABELS="$(nproc --all) vCPUs"
-LABELS=${LABELS},"$(sudo dmidecode -t 19 | awk -F ': ' '/Range Size/{print $2}' | tail -n 1) RAM"
+LABELS=${LABELS},$(uname -ms)
+LABELS=${LABELS},$(grep '^model name' /proc/cpuinfo | head -1 | awk -F ': ' '{ print $2 }')
+LABELS=${LABELS},"$(nproc --all) vCPUs"
+LABELS=${LABELS},$(grep MemTotal /proc/meminfo | awk '{$2=$2/(1024^2); printf "%.2f GB RAM\n", $2;}')
 if [ $(command -v nvidia-smi) ]; then
-        LABELS=${LABELS},$(nvidia-smi --query-gpu=gpu_name --format=csv | tail -n 1)
-        LABELS=${LABELS},"$(nvidia-smi --query-gpu=memory.total --format=csv | tail -n 1 | awk '{print $1 / 1024}') GB GPU RAM"
+        LABELS=${LABELS},"gpu",$(nvidia-smi --query-gpu=gpu_name --format=csv,noheader,nounits | tail -n 1)
+        LABELS=${LABELS},$(nvidia-smi --query-gpu=memory.total --format=csv,noheader,nounits | tail -n 1 | awk '{$1=$1/1024; printf "%.2f GB GPU RAM\n", $1;}')
 fi
-# Add label for unique ID
+# Add label for workdir/unique ID
 LABELS=${LABELS},${_RUNNER_ID}
 
 echo "Runner labels: $LABELS"
