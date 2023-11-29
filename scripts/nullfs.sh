df34a360e90d3180f8e2cc4d8caedc0007e78401
Samuel Burnham <45365069+samuelburnham@users.noreply.github.com>
2023-11-29 16:30:12 -0500

    Address feedback

--- scripts/nullfs.sh
+++ scripts/nullfs.sh
@@ -13,11 +13,11 @@ sudo dpkg -i ../nullfsvfs_0.17_amd64.deb
 # Set up nullfs
 sudo modprobe nullfs
 echo nullfs | sudo tee /etc/modules-load.d/nullfs.conf
-NFS_USERNAME=$(whoami)
-NFS_UID=$(id -u $NFS_USERNAME)
-NFS_GID=$(id -g $NFS_USERNAME)
+NULLFS_USERNAME=$(whoami)
+NULLFS_UID=$(id -u $NULLFS_USERNAME)
+NULLFS_GID=$(id -g $NULLFS_USERNAME)
 sudo mkdir -p /var/tmp/runner/lurk
-echo "none /var/tmp/runner/lurk nullfs default,uid=$NFS_UID,gid=$NFS_GID 0 0" | sudo tee -a /etc/fstab
+echo "none /var/tmp/runner/lurk nullfs default,uid=$NULLFS_UID,gid=$NULLFS_GID 0 0" | sudo tee -a /etc/fstab
 
 # Mount nullfs to the Docker runner
 echo "      - '/var/tmp/runner/lurk:/root/.lurk'" >> gh-actions-runner-compose.yml
