8dde0f54c1ef862a2bcf8be750c46e84b6c27183
Samuel Burnham <45365069+samuelburnham@users.noreply.github.com>
2023-12-01 22:50:05 -0500

    Replace deprecated `ubuntu-drivers autoinstall`

--- scripts/gpu_drivers.sh
+++ scripts/gpu_drivers.sh
@@ -3,6 +3,7 @@
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install ubuntu-drivers-common -y
 sudo ubuntu-drivers devices
-sudo ubuntu-drivers autoinstall
+# Edit with the desired driver number (e.g. 535)
+sudo ubuntu-drivers install <driver>
 
 # Then reboot and run `nvidia-smi`
