8ff0c5415a6372309bd88900092252552ef78ac5
Samuel Burnham <45365069+samuelburnham@users.noreply.github.com>
2024-01-25 10:25:54 -0500

    Pin GPU driver and kernel (#27)
    * Pin GPU driver and kernel
    
    * Address feedback

--- scripts/gpu_drivers.sh
+++ scripts/gpu_drivers.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+# Disable popups to restart services after `apt-get upgrade` by restarting them automatically
+sudo sed -i "s/\#\$nrconf{restart} = 'i';/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
+# Don't install suggested or recommended packages with `apt`
+printf "APT::Install-Suggests "0";\nAPT::Install-Recommends "0";" >> /etc/apt/apt.conf.d/99Recommended
+
 sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install ubuntu-drivers-common -y
-sudo ubuntu-drivers devices
-# Edit with the desired driver number (e.g. 535)
-sudo ubuntu-drivers install <driver>
+# Pin kernel version to prevent driver/kernel mismatches
+sudo apt-mark hold linux-generic
+
+# `alsa-utils` is needed to fix an `aplay` error when running `ubuntu-drivers devices`
+sudo apt-get install ubuntu-drivers-common alsa-utils -y
+# Edit with the desired driver version based on `ubuntu-drivers devices`
+NVIDIA_DRIVER_VERSION=$(sudo ubuntu-drivers devices | grep "recommended" | awk -F'-' '{ print $3 }')
+
+sudo ubuntu-drivers install $NVIDIA_DRIVER_VERSION
+# Pin driver version to prevent driver/kernel mismatches
+sudo apt-mark hold nvidia-driver-$NVIDIA_DRIVER_VERSION
 
 # Then reboot and run `nvidia-smi`
